1a7cf139da885c3948b72f63cec652c7a7a5cae1
Yasmin Vitória <88351564+Yav01fat@users.noreply.github.com>
2025-03-05 03:03:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       let piece = null;
 
       if (row < 3 && isBlack) {
-        piece = <div className={styles.piece} style={{ backgroundColor: '#FF69B4' }}></div>; // Peças azuis
+        piece = <div className={styles.piece} style={{ backgroundColor: '#FF69B4' }}></div>; // 
       } else if (row > 4 && isBlack) {
-        piece = <div className={styles.piece} style={{ backgroundColor: '#00BFFF' }}></div>; // Peças verde-claras
+        piece = <div className={styles.piece} style={{ backgroundColor: '#00BFFF' }}></div>; // 
       }
 
       rowSquares.push(
