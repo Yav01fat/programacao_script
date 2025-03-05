6f79f3429c0e9efb1b09d249293189d9d8738d93
Yav Fat <yayasilva032019@gmail.com>
2025-03-05 02:51:23 -0300

    Trabalho_Dama

---
+++ README.md
@@ -0,0 +1,34 @@
+# Projeto Tabuleiro de Damas
+
+## Como Rodar o Projeto
+
+1. **Clone o repositório:**
+
+   ```bash
+   git clone [https://github.com/dolthub/dolt](https://github.com/dolthub/dolt)
+   cd[Trabalho_Dama]
+
+2. Instale as dependências:
+
+npm install 
+# ou
+yarn install
+
+3. Inicie o servirdor do desenvolvimento
+
+npm run dev
+# ou
+yarn dev
+
+4. Abra o navegador: 
+
+Acesse o projeto em http://localhost:3006. 
+
+Observações: 
+
+Certifique-se de ter o Node.js e o npm (ou yarn) instalados em sua máquina.
+O projeto será executado na porta 3006. Se a porta estiver em uso, o Next.js tentará usar outra porta disponível.
+
+🛠 Tecnologias utilizadas
+Next.js – Framework React
+CSS Modules
---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import styles from '../styles/Board.module.css';
+
+export default function Home() {
+  const boardSize = 8;
+  const board = [];
+
+  for (let row = 0; row < boardSize; row++) {
+    const rowSquares = [];
+    for (let col = 0; col < boardSize; col++) {
+      const isBlack = (row + col) % 2 === 1;
+      let piece = null;
+
+      if (row < 3 && isBlack) {
+        piece = <div className={styles.piece} style={{ backgroundColor: '#FF69B4' }}></div>; // Peças azuis
+      } else if (row > 4 && isBlack) {
+        piece = <div className={styles.piece} style={{ backgroundColor: '#00BFFF' }}></div>; // Peças verde-claras
+      }
+
+      rowSquares.push(
+        <div
+          key={`${row}-${col}`}
+          className={`${styles.square} ${isBlack ? styles.black : styles.white}`}
+        >
+          {piece}
+        </div>
+      );
+    }
+    board.push(<div key={row} className={styles.row}>{rowSquares}</div>);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1>Trabalho Tabuleiro Dama</h1>
+      <div className={styles.board}>{board}</div>
+    </div>
+  );
+
+  
+}
